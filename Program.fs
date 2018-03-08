196d9795ebd9a2ed9d1f0ef38630dc3a3bcb6bab
Ugo Mastracchio <ugo.mastracchio@zebra.com>
2018-03-08 21:09:22 +0100

    weblinkendpoint con mailboxprocessor per messaggi

--- Program.fs
+++ Program.fs
@@ -20,76 +20,38 @@ open Suave.WebSocket
 
 //TODO: https://github.com/SuaveIO/suave/issues/307
 
-type RegionEventType = 
- | EnterRegion
- | ExitRegion
-  
-[<DataContract>]
-type RegionEvent =
-   { 
-      [<field: DataMember(Name = "regionName")>]
-      regionName : string;
-      [<field: DataMember(Name = "deviceID")>]
-      deviceID : string;
-      [<field: DataMember(Name = "timeStamp")>]
-      timeStamp : int64;
-      [<field: DataMember(Name = "eventType")>]
-      eventType : RegionEventType;
-   }
-
-let regionEventProcessor func:WebPart = 
-   mapJson (fun (regEvent:RegionEvent) -> 
-                        func regEvent
-                        regEvent)
-
-let rec updateDeviceState re list =
-      match list with
-      | [] -> []
-      | reHead :: xs -> if reHead.deviceID = re.deviceID then re :: xs else (reHead :: updateDeviceState re xs)
- 
-type LocationAgentMsg = 
+   
+type DumpAgentMsg = 
     | Exit
     | Reset
-    | UpdateWith of RegionEvent
-    | IsKnownDevice of String * AsyncReplyChannel<Boolean>
+    | UpdateWith of String
     | DumpDevicesState  of AsyncReplyChannel<String>
 
-type DevicesState = 
-   { LastRegionEventPerDevice : RegionEvent list } 
-   static member Empty = {LastRegionEventPerDevice = [] }
-   member x.IsKnownDevice address = 
-      List.exists (fun devLoc -> devLoc.deviceID = address) x.LastRegionEventPerDevice
-   member x.UpdateDeviceState re = updateDeviceState re x.LastRegionEventPerDevice
-   member x.UpdateWith (re:RegionEvent) = 
-      { LastRegionEventPerDevice = 
-          if x.IsKnownDevice re.deviceID then
-              x.UpdateDeviceState re
-          else
-              re :: x.LastRegionEventPerDevice}
+type MessageDump = 
+   { LastMessage : String } 
+   static member Empty = {LastMessage = "" }
+   member x.UpdateWith (re:String) = 
+      { LastMessage = re }
 
-type LocationAgent() =
-    let locationAgentMailboxProcessor =
+type PrinterMsgAgent() =
+    let printerMsgMailboxProcessor =
         MailboxProcessor.Start(fun inbox ->
             let rec locationAgentLoop dsi =
                 async { let! msg = inbox.Receive()
                         match msg with
                         | Exit -> return ()
-                        | Reset -> return! locationAgentLoop DevicesState.Empty
+                        | Reset -> return! locationAgentLoop MessageDump.Empty
                         | UpdateWith re -> return! locationAgentLoop (dsi.UpdateWith re)
-                        | IsKnownDevice (addr, replyChannel) -> 
-                            replyChannel.Reply (dsi.IsKnownDevice addr)
-                            return! locationAgentLoop dsi
                         | DumpDevicesState replyChannel -> 
                             replyChannel.Reply (sprintf "%A" dsi)
                             return! locationAgentLoop dsi
                       }
-            locationAgentLoop DevicesState.Empty
+            locationAgentLoop MessageDump.Empty
         )
-    member this.Exit() = locationAgentMailboxProcessor.Post(Exit)
-    member this.Empty() = locationAgentMailboxProcessor.Post(Reset)
-    member this.UpdateWith re = locationAgentMailboxProcessor.Post(UpdateWith re)
-    member this.IsKnownDevice addr = locationAgentMailboxProcessor.PostAndReply((fun reply -> IsKnownDevice(addr,reply)), timeout = 2000)
-    member this.DumpDevicesState() = locationAgentMailboxProcessor.PostAndReply((fun reply -> DumpDevicesState reply), timeout = 2000)
+    member this.Exit() = printerMsgMailboxProcessor.Post(Exit)
+    member this.Empty() = printerMsgMailboxProcessor.Post(Reset)
+    member this.UpdateWith re = printerMsgMailboxProcessor.Post(UpdateWith re)
+    member this.DumpDevicesState() = printerMsgMailboxProcessor.PostAndReply((fun reply -> DumpDevicesState reply), timeout = 2000)
 
 let config = 
     let port = System.Environment.GetEnvironmentVariable("PORT")
@@ -101,18 +63,13 @@ let config =
                     else HttpBinding.create HTTP ipZero (uint16 port)) ] }
 
 
-let ws (webSocket : WebSocket) (context: HttpContext) =
+let ws (mAgent:PrinterMsgAgent) (webSocket : WebSocket) (context: HttpContext) =
   socket {
-    let msg1 = "message from Heroku server"
-    // the response needs to be converted to a ByteSegment
-    let byteResponse =
-        msg1
-        |> System.Text.Encoding.ASCII.GetBytes
-        |> ByteSegment
-    do! webSocket.send Text byteResponse true
 
     // if `loop` is set to false, the server will stop receiving messages
     let mutable loop = true
+
+    do mAgent.UpdateWith "in ws pre loop"
 
     while loop do
       // the server will wait for a message to be received without blocking the thread
@@ -130,16 +87,18 @@ let ws (webSocket : WebSocket) (context: HttpContext) =
       | (Text, data, true) ->
         // the message can be converted to a string
         let str = UTF8.toString data
-        let response = sprintf "Ugo su Heroku: received %s" str
-
-        // the response needs to be converted to a ByteSegment
-        let byteResponse =
-          response
-          |> System.Text.Encoding.ASCII.GetBytes
-          |> ByteSegment
+        let response = sprintf "Ugo su Heroku received: %s" str
 
         // the `send` function sends a message back to the client
-        do! webSocket.send Text byteResponse true
+        do mAgent.UpdateWith response
+
+      | (Binary, data, true) ->
+        // the message can be converted to a string
+        let str = UTF8.toString data
+        let response = sprintf "Ugo su Heroku received: %s" str
+
+        // the `send` function sends a message back to the client
+        do mAgent.UpdateWith response
 
       | (Close, _, _) ->
         let emptyResponse = [||] |> ByteSegment
@@ -152,10 +111,10 @@ let ws (webSocket : WebSocket) (context: HttpContext) =
     }
 
 /// An example of explictly fetching websocket errors and handling them in your codebase.
-let wsWithErrorHandling (webSocket : WebSocket) (context: HttpContext) = 
+let wsWithErrorHandling (mAgent:PrinterMsgAgent) (webSocket : WebSocket) (context: HttpContext) = 
    
    let exampleDisposableResource = { new IDisposable with member __.Dispose() = printfn "Resource needed by websocket connection disposed" }
-   let websocketWorkflow = ws webSocket context
+   let websocketWorkflow = ws mAgent webSocket context
    
    async {
     let! successOrError = websocketWorkflow
@@ -171,26 +130,23 @@ let wsWithErrorHandling (webSocket : WebSocket) (context: HttpContext) =
     return successOrError
    }
 
-let app (locationAgent:LocationAgent) : WebPart = 
+let app (mAgent:PrinterMsgAgent) : WebPart = 
   choose [
-    path "/websocket" >=> handShake ws
+    path "/websocket" >=> handShake (ws mAgent)
     // path "/websocketWithSubprotocol" >=> handShakeWithSubprotocol (chooseSubprotocol "test") ws
-    path "/websocketWithError" >=> handShake wsWithErrorHandling
+    path "/websocketWithError" >=> handShake (wsWithErrorHandling mAgent)
     GET >=> choose 
-        [ path "/" >=> file "index.html"
-          path "/hello" >=> OK "Hello GET"
-          //https://stackoverflow.com/questions/40561394/f-suave-warbler-function
-          path "/RegionStatusReport" >=> warbler (fun ctx -> OK ( locationAgent.DumpDevicesState() ))
+        [ path "/hello" >=> OK "Hello GET"
+          path "/dump" >=> warbler (fun ctx -> OK ( mAgent.DumpDevicesState() ))
           browseHome ]
-    POST >=> choose
-        [ path "/hello" >=> OK "Hello POST"
-          path "/regionEvent" >=> regionEventProcessor locationAgent.UpdateWith  ]  
     NOT_FOUND "Found no handlers." ]
+
+//https://help.heroku.com/tickets/560930
 
 [<EntryPoint>]
 let main _ =
-  let locationAgent = new LocationAgent()
-  startWebServer config (app locationAgent)
+  let msgAgent = new PrinterMsgAgent()
+  startWebServer config (app msgAgent)
   0
 
 //
@@ -207,18 +163,3 @@ let main _ =
 // More information on the WebSocket protocol can be found at: https://tools.ietf.org/html/rfc6455#page-34
 //
 
-//E:\Documents\weblinkendpoint>heroku certs:add ZebraCAChain.cer weblinkendpoint.herokuapp.com.cer weblinkendpoint.herokuapp.com.key
-//Resolving trust chain... done
-//Adding SSL certificate to ? weblinkendpoint... done
-//Certificate details:
-//Common Name(s): weblinkendpoint.herokuapp.com
-//Expires At:     2037-12-07 16:23 UTC
-//Issuer:         /CN=zebradevice1
-//Starts At:      2012-12-13 12:28 UTC
-//Subject:        /C=IT/ST=ZEBRAIT/L=MILAN/O=ZEBRA/OU=ZIT25/CN=weblinkendpoint.herokuapp.com/emailAddress=ugo.mastracchio@zebra.com
-//SSL certificate is not trusted.
-//
-//=== The following common names are for hosts that are managed by Heroku
-//weblinkendpoint.herokuapp.com
-//
-//=== Your certificate has been added successfully.  Add a custom domain to your app by running ? heroku domains:add <yourdomain.com>
