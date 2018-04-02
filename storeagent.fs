e8e8a04270b96b0440b9a82f2ae5cc35de2c273e
Ugo Mastracchio <ugo.mastracchio@zebra.com>
2018-04-02 21:20:05 +0200

    jsonhelper

--- storeagent.fs
+++ storeagent.fs
@@ -10,18 +10,8 @@ open System.IO
 open System.Xml
 open System.Text
 
+open JsonHelper
 
-/// Object to Json 
-let internal json<'t> (myObj:'t) =   
-        use ms = new MemoryStream() 
-        (new DataContractJsonSerializer(typeof<'t>)).WriteObject(ms, myObj) 
-        Encoding.Default.GetString(ms.ToArray()) 
-
-/// Object from Json 
-let internal unjson<'t> (jsonString:string)  : 't =  
-        use ms = new MemoryStream(ASCIIEncoding.Default.GetBytes(jsonString)) 
-        let obj = (new DataContractJsonSerializer(typeof<'t>)).ReadObject(ms) 
-        obj :?> 't
 
 [<DataContract>]
 type Product =
