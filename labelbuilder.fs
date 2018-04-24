d7a5fbc571396d090bb67829ea8ae493410d2611
Ugo Mastracchio <ugo.mastracchio@zebra.com>
2018-04-24 14:55:53 +0200

    LH10

--- labelbuilder.fs
+++ labelbuilder.fs
@@ -50,7 +50,7 @@ let convertIfadLabel (label:string) =
     let index6 = label.IndexOf("^FO150,305^ADN30,35^FD") + "^FO150,305^ADN30,35^FD".Length // 000000054986
     let field6 = label.Substring (index6, label.IndexOf("^FS", index6) - index6)
 
-    "^XA^BY2,2,10^LH0,20^FO13,7^ADN53,33^FD" + field1 + "^FS" +
+    "^XA^BY2,2,10^LH10,20^FO13,7^ADN53,33^FD" + field1 + "^FS" +
     // "^XA^BY2,2,10^LH20,20^FO13,7^ADN53,33^FD" + "CONVERTED LABEL" + "^FS" +
     "^FO08,53^A0N22,25^FD" + field2 + "^FS" + 
     "^FO08,80^ADN20,7^FD" + field3 + "^FS" +
