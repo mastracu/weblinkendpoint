f40c85129be6f434646930ea4a2393d635b20461
Ugo Mastracchio <ugo.mastracchio@zebra.com>
2018-11-03 18:38:36 +0100

    reading defaultinventory.json by using ReadAllText

--- storeagent.fs
+++ storeagent.fs
@@ -89,7 +89,7 @@ type StoreAgent() =
                       }
             // http://fsharp.github.io/FSharp.Data/library/Http.html
             // let defaultjson = Http.RequestString("http://weblinkendpoint.mastracu.it/defaultinventory.json")
-            let defaultjson = Http.RequestString("defaultinventory.json")
+            let defaultjson =File.ReadAllText("defaultinventory.json")
             let newStore = { ProductList = Array.toList (unjson<Product array> defaultjson) } 
             storeAgentLoop newStore
 
