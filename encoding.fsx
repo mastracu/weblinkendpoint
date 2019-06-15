e5a826888e961b3a9fb161da08a4c3589396ee9d
Ugo Mastracchio <ugo.mastracchio@zebra.com>
2019-06-15 19:05:37 +0200

    cleanup

--- encoding.fsx
+++
@@ -1,8 +0,0 @@
-
-let parolaIT = "città"B;;
-let defaultTable = System.Text.Encoding.Default.GetString parolaIT;;
-let table850 = System.Text.Encoding.GetEncoding(850).GetString parolaIT;;
-let table1252 = System.Text.Encoding.GetEncoding(1252).GetString parolaIT;;
-let table1250 = System.Text.Encoding.GetEncoding(1250).GetString parolaIT;;
-let utf8 = System.Text.Encoding.UTF8.GetString parolaIT;
-printfn "%d" System.Text.Encoding.Default.CodePage
